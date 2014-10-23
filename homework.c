4c720e4eabc94741ecd2e9ad2f2cedf180e070dd
hemanth-s-a <hemanth.s.a@gmail.com>
2014-10-22 20:47:34 -0400

    HW2

--- homework.c
+++ homework.c
@@ -1,309 +1,148 @@
-/*
+/* 
  * file:        homework.c
- * description: Skeleton for homework 1
+ * description: Skeleton code for CS 5600 Homework 2
  *
- * CS 5600, Computer Systems, Northeastern CCIS
- * Peter Desnoyers, Jan. 2012
- * $Id: homework.c 500 2012-01-15 16:15:23Z pjd $
+ * Peter Desnoyers, Northeastern CCIS, 2011
+ * $Id: homework.c 530 2012-01-31 19:55:02Z pjd $
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <math.h>
+#include "hw2.h"
 
-#include "uprog.h"
-
-/***********************************/
-/* DECLARATIONS FOR code in misc.c */
-/***********************************/
-
-typedef int *stack_ptr_t;
-extern void init_memory(void);
-extern void do_switch(stack_ptr_t *location_for_old_sp, stack_ptr_t new_value);
-extern stack_ptr_t setup_stack(int *stack, void *func);
-extern int get_term_input(char *buf, size_t len);
-extern void init_terms(void);
-
-extern void  *proc1;
-extern void  *proc1_stack;
-extern void  *proc2;
-extern void  *proc2_stack;
-extern void **vector;
-
-stack_ptr_t main_stack = NULL;
-
-int readfile(char *file, char *buf, int buflen);
-char *strword(char *s, char *buf);
-
-/***********************************************/
-/********* Your code starts here ***************/
-/***********************************************/
+/********** YOUR CODE STARTS HERE ******************/
 
 /*
- * Intialize vector table
- * This function initializes all the methods that OS provides
- * through a vector table which is stored at constant memory
- * location.
+ * Here's how you can initialize global mutex and cond variables
  */
-void init_vector(void)
-{
-  *(vector+0) = print;
-  *(vector+1) = readline;
-  *(vector+2) = getarg;
-  *(vector+3) = yield12;
-  *(vector+4) = yield21;
-  *(vector+5) = uexit;
-}
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t C_Barber = PTHREAD_COND_INITIALIZER;
+pthread_cond_t C_Customer = PTHREAD_COND_INITIALIZER;
+pthread_cond_t C_Haircut = PTHREAD_COND_INITIALIZER;
+int number_of_customers = 0;
+int max_customers = 5;
+int time_delay = 3; //average time between customer() calls 
 
-/*********************************************/
+int total_customers = 0;
+int total_turned_away = 0;
 
-/*
- * Question 1.
- *
- * The micro-program q1prog.c has already been written, and uses the
- * 'print' micro-system-call (index 0 in the vector table) to print
- * out "Hello world".
- *
- * You'll need to write the (very simple) print() function below, and
- * then put a pointer to it in vector[0].
- *
- * Then you read the micro-program 'q1prog' into memory starting at
- * address 'proc1', and execute it, printing "Hello world".
- *
+/* the barber method
  */
-void print(char *line)
+void barber(void)
 {
-    /*
-     * Your code goes here. 
-     */
-  printf("%s", line);
-}
-
-void q1(void)
-{
-    /*
-     * Your code goes here. Initialize the vector table, load the
-     * code, and go.
-     */
-  if (readfile("q1prog", (char*) proc1, 106) != 0)
-    printf("Problem loading program\n");
-  init_vector();
-  int (*a)(void) = NULL;
-  a = proc1;
-  a();
-}
-
-int readfile(char *file, char *buf, int buflen)
-{
-    FILE *fp = fopen(file, "r");
-    if (fp == NULL) {
-	perror("can't open file");
-	return 0;
+    pthread_mutex_lock(&m);
+    while (1) {
+        /* your code here */
+      if (number_of_customers == 0) {
+	printf("DEBUG: %f barber goes to sleep\n", timestamp());
+	pthread_cond_wait(&C_Barber, &m);
+	printf("DEBUG: %f barber wakes up\n", timestamp());
+      }
+      // add a sleep
+      double T = 1.2;
+      sleep_exp(T, &m);
+      pthread_cond_signal(&C_Haircut);
     }
-    int i, c;
-    for (i = 0; i < buflen; i++)
-      {
-	c = getc(fp);
-	if (c == EOF)
-		break;
-	buf[i] = c;
-    }
-    fclose(fp);
-    return i;
+    pthread_mutex_unlock(&m);
 }
 
-char *strword(char *s, char buf[])
-{
-    char *str = NULL;
-    str = strchr(s, ' ');
-    if(str != NULL) {
-      memcpy(buf, s, str-s);
-      s = str+1;
-    } else {
-      strcpy(buf, s);
-      s = NULL;
-    }
-    return s;
-}
-
-/*
- * Question 2.
- *
- * Add two more functions to the vector table:
- *   void readline(char *buf, int len) - read a line of input into 'buf'
- *   char *getarg(int i) - gets the i'th argument (see below)
-
- * Write a simple command line which prints a prompt and reads command
- * lines of the form 'cmd arg1 arg2 ...'. For each command line:
- *   - save arg1, arg2, ... in a location where they can be retrieved
- *     by 'getarg'
- *   - load and run the micro-program named by 'cmd'
- *   - if the command is "quit", then exit rather than running anything
- *
- * Note that this should be a general command line, allowing you to
- * execute arbitrary commands that you may not have written yet. You
- * are provided with a command that will work with this - 'q2prog',
- * which is a simple version of the 'grep' command.
- *
- * NOTE - your vector assignments have to mirror the ones in vector.s:
- *   0 = print
- *   1 = readline
- *   2 = getarg
+/* the customer method
  */
-void readline(char *buf, int len) /* vector index = 1 */
+void customer(int customer_num)
 {
-    /*
-     * Your code here.
-     */
-  int i = 0;
-  char c;
-  FILE *fp = stdin;
-  while((c = getc(fp)) != 13 && c != 10 && i < len) {
-    buf[i++] = c;
-  }
-  buf[i] = '\0';
+    pthread_mutex_lock(&m);
+    /* your code here */
+    printf("DEBUG: %f customer %d enters shop\n", timestamp(), customer_num);
+    ++total_customers;
+    if (number_of_customers >= max_customers) {
+      //printf("DEBUG: %f customer %d exits: shop is full\n", timestamp(), customer_num);
+      ++total_turned_away;
+    } 
+    else {
+      ++number_of_customers;
+      if (number_of_customers > 1) {
+	pthread_cond_wait(&C_Customer, &m);
+      }
+      pthread_cond_signal(&C_Barber);
+      printf("DEBUG: %f customer %d starts haircut\n", timestamp(), customer_num);
+      pthread_cond_wait(&C_Haircut, &m);
+      --number_of_customers;
+
+      // indicate to other customers (if any) that there are no customers
+
+      pthread_cond_signal(&C_Customer);
+    }
+    printf("DEBUG: %f customer %d leaves shop\n", timestamp(), customer_num);
+    pthread_mutex_unlock(&m);
 }
 
-char *getarg(int i)		/* vector index = 2 */
-{
-    /*
-     * Your code here. 
-     */
-  char **args = *(char**)proc1_stack;
-  //printf("%s\n",*(args+i));
-  //return (char*)(proc1_stack+i);
-  return *(args+i);
-
-    return NULL;
-}
-
-/*
- * Note - see c-programming.pdf for sample code to split a line into
- * separate tokens. 
+/* Threads which call these methods. Note that the pthread create
+ * function allows you to pass a single void* pointer value to each
+ * thread you create; we actually pass an integer (the customer number)
+ * as that argument instead, using a "cast" to pretend it's a pointer.
  */
+
+/* the customer thread function - create 10 threads, each of which calls
+ * this function with its customer number 0..9
+ */
+void *customer_thread(void *context) 
+{
+    int customer_num = (int)context; 
+
+    /* your code goes here */
+    while(1) {
+      //double sleep_time = rand() % time_delay;
+      sleep_exp(10.0, NULL);
+      customer(customer_num);
+    }
+    return 0;
+}
+
+/*  barber thread
+ */
+void *barber_thread(void *context)
+{
+    barber(); /* never returns */
+    return 0;
+}
+
 void q2(void)
 {
-    /* YOUR code goes here */
-  size_t length_of_buf = 128;
-  char *buf = malloc(sizeof(char) * length_of_buf);
-  char *word_buffer = buf;
-  void *temp_vector = NULL;
-  char **words = NULL;
-  char *line = malloc(20*sizeof(char));
+    /* to create a thread:
+        pthread_t t; 
+        pthread_create(&t, NULL, function, argument);
+       note that the value of 't' won't be used in this homework
+    */
+
+    /* your code goes here */
+  pthread_t t_barber;
+  pthread_create(&t_barber, NULL, barber_thread, 0);
+  
+  pthread_t t_customer[10];
   int i;
-
-  words = malloc(10 * sizeof(char *));
+  
   for(i = 0; i < 10; i++) {
-    *(words+i) = malloc(20 * sizeof(char));
+    pthread_create(&t_customer[i], NULL, customer_thread, (void*)i);
   }
-
-  init_vector(); 
-  while (1) {
-	/* get a line */
-	/* split it into words */
-	/* if zero words, continue */
-	/* if first word is "quit", break */
-	/* make sure 'getarg' can find the remaining words */
-	/* load and run the command */
-    word_buffer = buf;
-    readline(buf, length_of_buf);
-      //readfile("q1prog", (char*) proc1, 106);
-    i = 0;
-    while(word_buffer != NULL) {
-      word_buffer = strword(word_buffer, *(words+i++));
-      //buf = strword(buf, (char*)proc1_stack);
-      //*proc1_stack = words+i++);
-    }
-    *(words+i) = 0;
-    *(char**)proc1_stack = words;
-
-    int ab = strcmp(*words, "quit");
-    //printf ("%d\n", ab);
-    if (strcmp(*words, "quit") == 0)
-      break;
-    //printf("will read file\n");
-    if(readfile(*words, (char*) proc1, 3000) == 0) {
-      printf("Command not found\n");
-      continue;
-    }
-    //printf("read file\n");
-    //print((char*)proc1);
-    int (*a)(void) = NULL;
-    a = proc1;
-    //printf("calling micro\n");
-    a();
-    //printf("control returned\n");
-  }
-    /*
-     * Note that you should allow the user to load an arbitrary command,
-     * and print an error if you can't find and load the command binary.
-     */
-  for(i = 0; i < 10; i++) {
-    free(*(words+i));
-  }
-  free(words);
+  wait_until_done();
 }
 
-/*
- * Question 3.
+/* For question 3 you need to measure the following statistics:
  *
- * Create two processes which switch back and forth.
+ * 1. fraction of  customer visits result in turning away due to a full shop 
+ *    (calculate this one yourself - count total customers, those turned away)
+ * 2. average time spent in the shop (including haircut) by a customer 
+ *     *** who does not find a full shop ***. (timer)
+ * 3. average number of customers in the shop (counter)
+ * 4. fraction of time someone is sitting in the barber's chair (counter)
  *
- * You will need to add another 3 functions to the table:
- *   void yield12(void) - save process 1, switch to process 2
- *   void yield21(void) - save process 2, switch to process 1
- *   void uexit(void) - return to original homework.c stack
- *
- * The code for this question will load 2 micro-programs, q3prog1 and
- * q3prog2, which are provided and merely consists of interleaved
- * calls to yield12() or yield21() and print(), finishing with uexit().
- *
- * Hints:
- * - Use setup_stack() to set up the stack for each process. It returns
- *   a stack pointer value which you can switch to.
- * - you need a global variable for each process to store its context
- *   (i.e. stack pointer)
- * - To start you use do_switch() to switch to the stack pointer for 
- *   process 1
+ * The stat_* functions (counter, timer) are described in the PDF. 
  */
 
-void yield12(void)		/* vector index = 3 */
+void q3(void)
 {
-    /* Your code here */
-  //setup_stack();
-  do_switch((stack_ptr_t*)&proc1_stack, proc2_stack);
+    /* your code goes here */
+  q2();
+  printf("Fraction of customers turned away: %f\n", 1.0*total_turned_away/total_customers);
 }
-
-void yield21(void)		/* vector index = 4 */
-{
-    /* Your code here */
-  do_switch((stack_ptr_t*)&proc2_stack, proc1_stack);
-}
-
-void uexit(void)		/* vector index = 5 */
-{
-    /* Your code here */
-  do_switch((stack_ptr_t*)&proc1_stack, main_stack);
-}
-
-void q3(void) 
-{
-    /* Your code here */
-    /* load q3prog1 into process 1 and q3prog2 into process 2 */
-
-  init_vector();
-  readfile("q3prog1", (char*)proc1, 3000);
-  readfile("q3prog2", (char*)proc2, 3000);
-  main_stack = malloc(sizeof(stack_ptr_t));
-  //main_stack = (void*)(main_stack + 4096 - 4);
-
-  proc1_stack = setup_stack(proc1_stack, proc1);
-  proc2_stack = setup_stack(proc2_stack, proc2);
-  do_switch(&main_stack, proc1_stack);
-
-}
-
-
-/***********************************************/
-/*********** Your code ends here ***************/
-/***********************************************/
